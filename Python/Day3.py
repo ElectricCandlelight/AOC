4c22b38abf3e95497590f16333612e1fdaf34b9b
ElectricCandlelight <ecandlelight@gmail.com>
2024-12-05 01:38:51 +0000

    Day3 Part2

--- Python/Day3.py
+++ Python/Day3.py
@@ -6,21 +6,36 @@ def read_input(data):
         return file.read()
 
 
-def find_mul(data):
-    return re.findall(r"mul\((\d+),(\d+)\)", data)
+def find_pattern(data):
+    pattern = re.compile(r"(do|don't|mul)\((\d*),?(\d*)\)")
+
+    matches = [(match.group(1), match.group(2), match.group(3), match.start())
+               for match in pattern.finditer(data)]
+
+    matches.sort(key=lambda match: match[3])
+
+    matches = [(match[0], match[1], match[2]) for match in matches]
+    return matches
 
 
-def multiply_add(data):
+def process_command(data):
     result = 0
-    for i in data:
-        result += int(i[0]) * int(i[1])
+    do = True
+    for command, x, y, in data:
+        if command == "mul":
+            if do:
+                result += int(x) * int(y)
+        elif command == "do":
+            do = True
+        elif command == "don't":
+            do = False
     return result
 
 
 def main():
     data = read_input("Day3.txt")
-    data = find_mul(data)
-    result = multiply_add(data)
+    operations = find_pattern(data)
+    result = process_command(operations)
     print(result)
 
 
